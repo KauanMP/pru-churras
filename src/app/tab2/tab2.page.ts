485afb66b29494f59483a4d9bbfc7405634030a1
KauanMP <kauanmoreira1232@gmail.com>
2023-03-03 22:21:42 -0300

    Criando um Sistema de jogo de truco

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,66 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  valendo = 1;
+  placarUm = 0;
+  placarDois = 0;
 
+  
+  constructor() {}
+  
+  MultiplicarUm () {
+    this.valendo = 1
+  }
+
+  MultiplicarTres () {
+    this.valendo = 3
+  }
+
+  MultiplicarSeis () {
+    this.valendo = 6
+  }
+
+  MultiplicarNove () {
+    this.valendo = 9
+  }
+
+  MultiplicarDoze () {
+    this.valendo = 12
+  }
+
+  BotaoSomaTime1 () {
+    this.placarUm += this.valendo
+    this.valendo = 1
+  }
+
+  BotaoSomaTime2 () {
+    this.placarDois += this.valendo
+    this.valendo = 1
+  }
+
+  BotaoSubtraiTime1 () {
+    this.placarUm -= this.valendo
+    this.valendo = 1
+
+    if (this.placarUm <= 0) {
+      this.placarUm = 0
+    }
+  }
+
+  BotaoSubtraiTime2 () {
+    this.placarDois -= this.valendo
+    this.valendo = 1
+
+    if (this.placarDois <= 0) {
+      this.placarDois = 0
+    }
+  }
+  
+  PlacarTime1 () {
+
+  }
+
+  PlacarTime2 () {
+    
+  }
 }
